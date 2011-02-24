9ec9e5ab4f2bc27f1460f35e5eba026729d0765e
Bruno Oliveira <bruno@abstractj.com>
2011-02-24 13:18:10 -0300

    Changing debug

--- lib/restfulie/client/http/link_header.rb
+++ lib/restfulie/client/http/link_header.rb
@@ -5,7 +5,7 @@ module Restfulie::Client::HTTP
     
     def links
       r = link_header_to_array
-			LinkHeader.debug r
+			LinkHeader.debug "Debugging this piece of code #{r}"
       Medie::Xml::Links.new(r)
     end
     
